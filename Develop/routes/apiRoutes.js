acae46021fd74a00b106cb8011300b5bdf525185
willsan0723 <william.santee@gmail.com>
2020-10-11 15:46:22 -0600

    got get and post methods to function properly

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,24 +1,21 @@
 const router = require('express').Router();
-const {notes} = require('../db/db.json');
-const store = require('../db/store')
+const { response } = require('express');
+const { notes } = require('../db/db.json');
+// const store = require('../db/store')
+
+// /api/notes
 
 router.get('/notes', (req, res) => {
-    store.getNotes().then(notes => {
-        res.json(notes);
-    }).catch(err => {
-        console.log(notes);
-        res.status(500).json(err)
-    })
-    // let display = notes;
-    // res.json(display);
+    let results = notes;
+    res.json(results);
 })
 
+router.post('/notes', (req, res) => {
+    notes.push(req.body)
+    res.json(notes);
+})
 
-// router.post('/notes', (req, res) => {
-//     saveNote(req.body)
-//     .then((note) => res.json(note))
-//     .catch((err) => res.status(500).json(err)) 
-// })
+// add synchronously
 
 // router.delete('/notes/:id', (req, res) => {
 //     deleteNote(req.params.id)
